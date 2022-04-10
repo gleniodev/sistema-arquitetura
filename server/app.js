51d7e982c491ec805af8b2f158a36ab59a219861
Glenio Anderson <gleniodev@gmail.com>
2022-04-10 09:26:56 -0300

    Rotas de portfólio adicionadas

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const categoriasRouter = require('./routes/categorias');
+const clientesRouter = require('./routes/clientes');
+const contatoRouter = require('./routes/contato');
+const loginRouter = require('./routes/login');
+const portfolioRouter = require('./routes/portfolio');
+const usuariosRouter = require('./routes/usuarios');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,16 +23,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/categorias', categoriasRouter);
+app.use('/clientes', clientesRouter);
+app.use('/contato', contatoRouter);
+app.use('/login', loginRouter);
+app.use('/portfolio', portfolioRouter);
+app.use('/usuarios', usuariosRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ server/routes/portfolio.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const portfolioController = require('../controllers/portfolio.js')
+
+//LISTAR TODOS OS PROJETOS
+router.get('/', portfolioController.index)
+
+//LISTAR PROJETOS POR ID
+router.get('/:id', portfolioController.show)
+
+//LISTAR PROJETOS POR CATEGORIA
+router.get('/categoria/:id', portfolioController.categoria)
+
+//LISTAR PROJETOS POR CLIENTE
+router.get('/cliente/:id', portfolioController.cliente)
+
+//CADASTRAR NOVO PROJETO
+router.post('/cadastrar', portfolioController.add)
+
+//EDITAR PROJETO
+router.put('/editar/:id', portfolioController.edit)
+
+//EXCLUIR PROJETO
+router.delete('/excluir/:id', portfolioController.delete)
+
+module.exports = router
